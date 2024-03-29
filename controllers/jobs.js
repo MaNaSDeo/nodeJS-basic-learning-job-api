dca6f295eaacc13dafa7e0c8ed1724c9d14aede8
MaNaSDeo <rmnslogin@gmail.com>
2024-03-29 22:21:29 +0530

    Get single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -8,7 +8,21 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("Get One Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with jobId ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
