e5fea1f69896b769987565c357da1458b5305eff
MaNaSDeo <rmnslogin@gmail.com>
2024-03-29 23:55:07 +0530

    Cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -11,7 +11,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
   if (err.name === "ValidationError") {
-    console.log();
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(", ");
@@ -23,6 +22,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       err.keyValue
     )} field, please choose another value`;
     customError.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = 404;
   }
 
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
